ecaa8e375e84e2c4b0d01a84410707d41b2d5731
Khamid Shapkatov <k_shapkatov@kbtu.kz>
2026-04-27 18:51:54 +0500

    final fix template

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template
 import re
+import os
 
 app = Flask(__name__)
 
@@ -25,7 +26,6 @@ def analyze_url(url):
     if "-" in url and "secure" in url:
         score += 2
 
-    # уровень риска
     if score >= 5:
         return "🔴 High Risk (Phishing likely)"
     elif score >= 3:
@@ -33,72 +33,6 @@ def analyze_url(url):
     else:
         return "🟢 Low Risk (Safe)"
 
-
-HTML = """
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Phishing Detector</title>
-    <style>
-        body {
-            font-family: Arial;
-            background: linear-gradient(to right, #141e30, #243b55);
-            color: white;
-            text-align: center;
-            padding-top: 50px;
-        }
-        input {
-            padding: 12px;
-            width: 300px;
-            border-radius: 8px;
-            border: none;
-        }
-        button {
-            padding: 12px 20px;
-            border-radius: 8px;
-            border: none;
-            background: #00c6ff;
-            color: white;
-            cursor: pointer;
-            font-size: 16px;
-        }
-        .box {
-            background: rgba(255,255,255,0.1);
-            padding: 30px;
-            border-radius: 15px;
-            display: inline-block;
-        }
-        .history {
-            margin-top: 30px;
-            font-size: 14px;
-        }
-    </style>
-</head>
-<body>
-    <div class="box">
-        <h1>🔐 Phishing Detection Tool</h1>
-        <p>Check if a website is safe</p>
-
-        <form method="POST">
-            <input type="text" name="url" placeholder="Enter URL" required>
-            <br><br>
-            <button type="submit">Check</button>
-        </form>
-
-        <h2>{{ result }}</h2>
-
-        <div class="history">
-            <h3>History</h3>
-            {% for item in history %}
-                <p>{{ item }}</p>
-            {% endfor %}
-        </div>
-
-        <p style="font-size: 12px;">This tool uses rule-based detection</p>
-    </div>
-</body>
-</html>
-"""
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -113,9 +47,8 @@ def home():
         if len(history) > 5:
             history.pop()
 
-    return render_template_string(HTML, result=result, history=history)
+    return render_template("index.html", result=result, history=history)
 
-import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
