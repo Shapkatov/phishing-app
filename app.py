f9fef7fe1fb11e62964900da4b20ed19794659c5
Khamid Shapkatov <k_shapkatov@kbtu.kz>
2026-04-27 18:33:56 +0500

    fix port

--- app.py
+++ app.py
@@ -115,7 +115,5 @@ def home():
 
     return render_template_string(HTML, result=result, history=history)
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
-    
+    app.run(host="0.0.0.0", port=10000)
